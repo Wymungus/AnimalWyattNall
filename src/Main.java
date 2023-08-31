98cfd932f594e0d0fb2519ea90768f5aa115a6ca
100153234 <100153234@lubbockisd.net>
2023-08-31 09:49:00 -0500

    trying to push

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("             /)  (\\");
+        System.out.println("        .-._((,~~.))_.-,\n" +
+                "         `=.   99   ,='");
+System.out.println("           / ,o~~o. \\\n" +
+        "          { { .__. } }");
+System.out.println("           ) `~~~\\' (\n" +
+        "          /`-._  _\\.-\\");
+System.out.println("         /         )  \\\n" +
+        "       ,-X        #   X-.");
+System.out.println("hjw   /   \\          /   \\\n" +
+        "     (     )| |  | |(     )");
+System.out.println("      \\   / | |  | | \\   /\n" +
+        "       \\_(.-( )--( )-.)_/");
+System.out.println("       /_,\\ ) /  \\ ( /._\\\n" +
+        "           /_,\\  /._\\");
+
+
     }
+
+
+
+
 }
